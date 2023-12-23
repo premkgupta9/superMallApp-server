734c694aa954c778ad37333ce7c51c5b942aed35
Bishal-Saud <115933710+Bishal-Saud@users.noreply.github.com>
2023-12-23 11:19:44 +0545

    checking branch

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ dotenv.config({
 connectDB()
 .then(() => {
     app.listen(process.env.PORT || 7000, () => {
-        console.log(` Server is running at port: ${process.env.PORT}`);
+        console.log(` Server is running at port -: ${process.env.PORT}`);
     })
 })
 .catch((err) => {
