3961c38e8c11e39a8cdcc13a01653b52d4b64538
Bishal-Saud <115933710+Bishal-Saud@users.noreply.github.com>
2023-12-27 15:57:59 +0545

    Committing local changes before merge

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,18 +1,25 @@
 import { User } from "../models/user.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
-import { asyncHandeler } from "../utils/asyncHandler.js";
+import asyncHandler from "../utils/asyncHandler.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 
-const userRegister = asyncHandeler(async (req, res) => {
-  const { email, phone, fullName, password } = req.body;
+const userRegister = asyncHandler(async (req, res, next) => {
+  // get user details from fronted
+  // validation
+  // check if user already exists
+  // check for avatar
+  // upload on cloudinary , avatar
+  // create user object
+  // remove password and refresh token from res
+  // check user creation
+  // return res
 
-  if (
-    [email, phone, fullName, password].some(
-      (field) => !field || field.trim() === ""
-    )
-  ) {
-    throw new ApiError(400, "All fields are required");
+  const { email, phone, fullName, password } = req.body;
+  // console.log("email: ", email);
+
+  if (!email || !phone || !fullName || !password) {
+    return next(new ApiError(400, "All fields are required"));
   }
 
   const existedUser = await User.findOne({
@@ -20,40 +27,37 @@ const userRegister = asyncHandeler(async (req, res) => {
   });
 
   if (existedUser) {
-    throw new ApiError(400, "User with email or phone already exists");
+    return next(new ApiError("user with email or phone is already exist,"));
   }
 
-  try {
-    const user = await User.create({
-      email,
-      phone,
-      fullName,
-      password,
-      avatar: {
-        public_id: email,
-        secure_url: "avatar.img.url",
-      },
-    });
+  const avatarLocalPath = req.files?.avatar[0]?.path;
 
-    console.log("req.file:", req.file);
+  const avatar = await uploadOnCloudinary(avatarLocalPath);
 
-    if (req.file) {
-      const localPath = req.file.avatar;
-      const avatar = await uploadOnCloudinary(localPath);
-      user.avatar = avatar?.url;
-    }
+  const user = await User.create({
+    email,
+    phone,
+    fullName,
+    password,
+    avatar: {
+      public_id: email,
+      secure_url: "img.url",
+    },
+  });
 
-    await user.save();
-    user.password = undefined;
-    user.refreshToken = undefined;
+  const createdUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+  );
 
-    res
-      .status(201)
-      .json(new ApiResponse(201, user, "User registered successfully"));
-  } catch (error) {
-    console.error("Error during user registration:", error);
-    throw new ApiError(400, "Error during user registration");
+  if (!createdUser) {
+    return next(
+      new ApiError(500, "Something went wrong while registering the user")
+    );
   }
+
+  return res
+    .status(201)
+    .json(new ApiResponse(201, createdUser, "User registered Successfully"));
 });
 
 export { userRegister };
