ded5aca84ac1ce176c6b15c8ea36f6d2c3405b66
prem k gupta <premg9554@gmail.com>
2024-01-03 16:54:57 +0530

    set routes

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,8 +1,17 @@
 import { Router } from "express";
 import { upload } from "../middlewares/multer.middleware.js"
-import { userRegister } from "../controllers/user.controller.js";
+import { changePassword, getUser, refreshAccessToken, updateAccontDetails, userLogin, userLogout, userRegister } from "../controllers/user.controller.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router()
 
-router.post("/register", upload.single("avatar"), userRegister)
+router.route("/register").post(upload.single("avatar"), userRegister) 
+router.route("/login").post(userLogin)
+
+router.route("/logout").post(verifyJWT, userLogout)
+    router.route("/refresh-token").post(refreshAccessToken)
+    router.route("/change-password").post(verifyJWT, changePassword)
+    router.route("/getUser").get(verifyJWT, getUser)
+    router.route("/update-account").patch(verifyJWT, updateAccontDetails)
+
 export default router
