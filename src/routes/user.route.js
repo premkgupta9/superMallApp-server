1d68ce389cd6941328707510453c5f2516a1c59e
Bishal-Saud <115933710+Bishal-Saud@users.noreply.github.com>
2024-01-07 13:03:32 +0545

    Revert "adminlogin setup"
    This reverts commit 32f14510a85c19fde6992a2b7e96336fea008008.

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,18 +1,17 @@
 import { Router } from "express";
 import { upload } from "../middlewares/multer.middleware.js"
-import {  adminLogin, changePassword, getUser, logout, refreshAccessToken, updateProfile, userLogin, userRegister } from "../controllers/user.controller.js";
+import { changePassword, getUser, refreshAccessToken, updateAccontDetails, userLogin, userLogout, userRegister } from "../controllers/user.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router()
 
 router.route("/register").post(upload.single("avatar"), userRegister) 
 router.route("/login").post(userLogin)
-router.route("/admin/login").post(adminLogin)
 
-router.route("/logout").post(verifyJWT, logout)
+router.route("/logout").post(verifyJWT, userLogout)
     router.route("/refresh-token").post(refreshAccessToken)
     router.route("/change-password").post(verifyJWT, changePassword)
     router.route("/getUser").get(verifyJWT, getUser)
-    router.route("/update-account").patch(verifyJWT, updateProfile)
+    router.route("/update-account").patch(verifyJWT, updateAccontDetails)
 
 export default router
